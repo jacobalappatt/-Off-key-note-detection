b0d6c48f78d12e2cb89ec63695c9132cf57eb6c2
jacobalappatt <jacob_alappatt@g.harvard.edu>
2021-11-18 15:58:08 -0500

    contour trials
    Fixed rms issue in generation
    
    Added half octave shift to second melody

--- generate_contourtrials.m
+++ generate_contourtrials.m
@@ -1,55 +1,73 @@
 % Second script to generate contour trials
-% First script was deleted so I removed the upload from mindhive and
-% replaced it with these trials. Committed to Git repo on 11/17/2021. 
+% Added rms normalization that actually works
+% Pitch shifting the second melody by half an octave
+% UNADDRESSED - Generating a lot of extra files - consider deleting the first merged copy 
+
+rng('shuffle');
 
 for i = 1:20
     dirname=num2str(i);
-    dir=['/Users/jacob/Desktop/audiofiles/ContourTrials2_11172021/Set',dirname];
+    dir=['/Users/jacob/Desktop/audiofiles/ContourTrials3_11182021/Set',dirname];
     mkdir(dir);
     
     cd(dir); 
     lookup_data=[];
+    
     for sample = 1:40
         
-    
-   
-melody = randsample([1 -1], 4, 1);
-note_to_change = randsample(2:3, 1);
-sample_num=num2str(sample); 
-condition = randsample([1,2],1)
+        melody = randsample([1 -1], 4, 1);
+        note_to_change = randsample(2:3, 1);
+        sample_num=num2str(sample); 
+        condition = randsample([1,2],1)
  
-if condition ==1 % ref and second are different
-  melody2 = melody;
-  melody2(note_to_change) = -melody2(note_to_change);
-else
-  melody2 = melody; % ref and second are the same 
-end
+        if condition ==1 % ref and second are different
+            melody2 = melody;
+            melody2(note_to_change) = -melody2(note_to_change);
+        else
+            melody2 = melody; % ref and second are the same 
+        end
  
-ref = [0, cumsum(melody)];
-second = [0, cumsum(melody2)];
+        ref = [0, cumsum(melody)];
+        second = [0, cumsum(melody2)];
 
-frequencies =[220.00, 233.08, 246.94, 261.63, 277.18, 293.66, 311.13, 329.63];
-Fzero_num=frequencies(randi([1 8]));
-ref_tones = GenerateTones_v1(ref, Fzero_num);
-second_tones=GenerateTones_v1(second, Fzero_num);
+        frequencies =[220.00, 233.08, 246.94, 261.63, 277.18, 293.66, 311.13, 329.63, 349.23, 369.99, 392.00, 415.30, 440.00, 466.16, 493.88, 523.25];
+        which_f0=randi([1 8]) % We want the reference F0 between A3-E4
+        half_oct_f0=(which_f0 + 6) % We want the second F0 half an octave, i.e. 6 half-steps above reference F0.
+        
+        Fzero_num=frequencies(which_f0);
+        Fzero_num_halfoct=frequencies(half_oct_f0);
+        
+        ref_tones = GenerateTones_v1(ref, Fzero_num);
+        second_tones=GenerateTones_v1(second, Fzero_num_halfoct);
 
-lookup_data=[lookup_data; sample, condition];
+        lookup_data=[lookup_data; sample, condition];
 
-ref_filename=['trial_',sample_num,'_ref.wav'];
-second_filename=['trial_',sample_num,'_second.wav'];
-fs=44100;
-%merged_wav = [ref_tones; zeros(fs * 1.5, size(ref_tones, 2)); second_tones]; % https://www.mathworks.com/matlabcentral/answers/318452-how-can-i-merge-to-wav-files-with-a-pause-of-3-seconds-between-them
+        ref_filename=['trial_',sample_num,'_ref.wav'];
+        second_filename=['trial_',sample_num,'_second.wav'];
+        fs=44100;
 
-% RMS normalization to 0.05
+%Writing the ref and second audio files
+        audiowrite(ref_filename,ref_tones,fs);
+        audiowrite(second_filename,second_tones,fs);
+        
+% Read files, merge them, and write merged file
+        audio1=audioread(ref_filename);
+        audio2=audioread(second_filename);
+        merged_data = [audio1; zeros(fs * 1.5, size(audio1, 2)); audio2];
+        merged_file=['trial_',sample_num,'_merged.wav'];
+        audiowrite(merged_file, merged_data, fs);
+        
+% Read merged file, rms normalize, write rms norm file
         ampMax = 0.05;
-        ref_output = ampMax*ref_tones/rms(ref_tones);
-        second_output = ampMax*second_tones/rms(second_tones);
-        audiowrite(ref_filename,ref_output,fs);
-        audiowrite(second_filename,second_output,fs);
-end
+        rms_file=['trial_',sample_num,'_merged_rms.wav'];
+        pause(0.1);
+        output=rmsnorm(audioread(merged_file));
+        pause(0.1);
+        audiowrite(rms_file, output, fs);
+    end
 
-T=array2table(lookup_data); % Some jugaad to get the array of strings in a writable form
-writetable(T,['LookupFileOrder_major.csv']);
-clear T lookup_data;
+    T=array2table(lookup_data); % Some jugaad to get the array of strings in a writable form
+    writetable(T,['LookupFileOrder_major.csv']);
+    clear T lookup_data;
 
 end
